c11c839cc051d8eea6fdf3f23b43348752c3bc5a
Randy Lai <randy.cs.lai@gmail.com>
2014-08-26 09:41:41 -0700

    Update zsh_local_install.sh

--- zsh_local_install.sh
+++ zsh_local_install.sh
@@ -15,21 +15,11 @@ NCURSES_VERSION=5.9
 mkdir -p $HOME/.local $HOME/zsh_tmp
 cd $HOME/zsh_tmp
 
-if [ ! -d $HOME/.local/include/ncurses ]; then
-    tar xvzf ncurses-${NCURSES_VERSION}.tar.gz
-    cd ncurses-${NCURSES_VERSION}
-    ./configure --prefix=$HOME/.local
-    make
-    make install
-    cd ..
-fi
-
-
 wget -O zsh-${ZSH_VERSION}.tar.bz2 http://sourceforge.net/projects/zsh/files/zsh/${ZSH_VERSION}/zsh-${ZSH_VERSION}.tar.bz2/download
 tar jxvf zsh-${ZSH_VERSION}.tar.bz2
 cd zsh-${ZSH_VERSION}
-./configure --prefix=$HOME/.local --enable-multibyte --enable-pcre CFLAGS="-I$HOME/.local/include -I$HOME/.local/include/ncurses" LDFLAGS="-L$HOME/.local/lib -L$HOME/.local/include/ncurses -L$HOME/.local/include"
-CPPFLAGS="-I$HOME/.local/include -I$HOME/.local/include/ncurses" LDFLAGS="-static -L$HOME/.local/include -L$HOME/.local/include/ncurses -L$HOME/.local/lib" make
+./configure --prefix=$HOME/.local --enable-multibyte --enable-pcre CFLAGS="-I$HOME/.local/include" LDFLAGS="-L$HOME/.local/lib -L$HOME/.local/include"
+CPPFLAGS="-I$HOME/.local/include" LDFLAGS="-static -L$HOME/.local/include -L$HOME/.local/lib" make
 make install
 
 cd $OLDWD
