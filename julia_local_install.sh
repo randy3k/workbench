71f6f742c713ced59c960fcf741c1140ea223116
Randy Lai <randy.cs.lai@gmail.com>
2014-12-11 01:32:53 -0800

    Update julia_local_install.sh

--- julia_local_install.sh
+++ julia_local_install.sh
@@ -3,17 +3,23 @@
 # exit on error
 set -e
 
-OLDWD=$PWD
+JULIA_VER=0.3.3
 
+OLDWD=$PWD
 cd $HOME/.local
-git clone https://github.com/JuliaLang/julia
+git clone https://github.com/JuliaLang/julia || ( 
+  cd julia
+  make clean
+  git reset
+  git pull
+)
 cd julia
-git checkout v0.3.2
+git checkout "v$JULIA_VER"
 echo 'OPENBLAS_DYNAMIC_ARCH = 0' >> Make.user
 make -j8
-make install prefix=~/.local/julia-0.3.2
+make install prefix=~/.local/julia-$JULIA_VER
 
-ln -sf $HOME/.local/julia-0.3.2/bin/julia $HOME/.local/bin/julia
+ln -sf $HOME/.local/julia-$JULIA_VER/bin/julia $HOME/.local/bin/julia
 
 cd "$OLDWD"
 echo "$HOME/.local/bin/julia is now available. You can optionally add $HOME/.local/bin to your PATH."
