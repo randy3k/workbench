a39f1afd9f3b2d9b25fb9b4d36400b4f4da6e625
Randy Lai <randy.cs.lai@gmail.com>
2016-05-13 17:51:58 -0400

    Update server-bootstrap at Fri May 13 17:51:58 EDT 2016

--- ruby_local_install.sh
+++ ruby_local_install.sh
@@ -9,3 +9,17 @@ cd ~/.ruby-build
 env PREFIX="$HOME/.local" ./install.sh
 cd -
 ~/.local/bin/ruby-build 2.3.1 ~/.local/ruby-2.3.1
+
+if [[ -z $(echo $PATH | grep -o "$HOME/.local/ruby-2.3.1/bin") ]]; then
+    export PATH="$HOME/.local/ruby-2.3.1/bin:$PATH"
+fi
+
+if [[ -f ~/.profile ]] && [[ -z `cat ~/.profile | grep '$HOME/.local/ruby-2.3.1/bin'` ]]; then
+cat >> ~/.profile <<'EOF'
+
+# ruby 2.3.1
+if [ -d "$HOME/.local/ruby-2.3.1/bin" ] ; then
+    export PATH="$HOME/.local/ruby-2.3.1/bin:$PATH"
+fi
+EOF
+fi
