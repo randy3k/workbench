90760a13a19504264c6e515614ed0d50e2df0510
Randy Lai <randy.cs.lai@gmail.com>
2016-05-23 18:12:12 -0400

    Update server-bootstrap at Mon May 23 18:12:12 EDT 2016

--- dropbox.sh
+++ dropbox.sh
@@ -4,3 +4,11 @@
 set -e
 
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+
+cd /home/cslai/.dropbox-dist/dropbox-lnx*
+
+for f in `find . -name "*.so*"`; do
+    echo $f
+    patchelf --set-interpreter  ~/.linuxbrew/lib/ld.so "$f"
+    patchelf --set-rpath  '$ORIGIN:'$PWD':/home/cslai/.linuxbrew/lib' "$f"
+done
