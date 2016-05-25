b0ccad3c19e5cd450e45a752ff22f842e79178b8
Randy Lai <randy.cs.lai@gmail.com>
2016-05-25 01:35:32 -0400

    Update server-bootstrap at Wed May 25 01:35:32 EDT 2016

--- dropbox.sh
+++ dropbox.sh
@@ -8,9 +8,10 @@ cd ~ && curl -L "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 curl -L "https://www.dropbox.com/download?dl=packages/dropbox.py" -o ~/.local/bin/dropbox
 chmod +x ~/.local/bin/dropbox
 
+sed -i -e '2s/^/export DISPLAY=\n/' ~/.dropbox-dist/dropboxd
+
 if [ "$(hostname)" == "marconi" ]; then
     sed -i -e '1s|^#!/usr/bin/python|#!/usr/bin/env python2|' ~/.local/bin/dropbox
-    sed -i -e '2s/^/export DISPLAY=\n/' ~/.dropbox-dist/dropboxd
 
     cd /home/cslai/.dropbox-dist/dropbox-lnx*
 
