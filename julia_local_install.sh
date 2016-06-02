c30a0747f75d7d09732dfd08677d22f4852c5e16
Randy Lai <randy.cs.lai@gmail.com>
2016-06-02 18:17:15 -0400

    Update server-bootstrap at Thu Jun  2 18:17:15 EDT 2016

--- julia_local_install.sh
+++ julia_local_install.sh
@@ -8,6 +8,7 @@ DIR=`mktemp -d`
 wget "$URL" -O "$DIR/julia.tar.gz"
 tar xzfv "$DIR/julia.tar.gz" -C "$DIR"
 JULIADIR=`find "$DIR" -maxdepth 2 -name "julia*" -type d | head -n 1`
-mv "$JULIADIR" "$HOME/.local/julia-v0.4.5"
-ln -s "$HOME/.local/julia-v0.4.5/bin/julia" "$HOME/.local/bin/julia"
+rm -r "$HOME/.local/julia"
+mv "$JULIADIR" "$HOME/.local/julia"
+ln -sf "$HOME/.local/julia/bin/julia" "$HOME/.local/bin/julia"
 rm -r "$DIR"
