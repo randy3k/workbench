5ee430987c9e3446344c1a5dcc0d3bebea5a1e12
Randy Lai <randy.cs.lai@gmail.com>
2016-06-11 12:31:18 -0400

    Update server-bootstrap at Sat Jun 11 12:31:18 EDT 2016

--- dropbox.sh
+++ dropbox.sh
@@ -3,6 +3,8 @@
 # exit on error
 set -e
 
+rm -r $HOME/.dropbox-dist || true
+
 cd ~ && curl -L "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 curl -L "https://www.dropbox.com/download?dl=packages/dropbox.py" -o ~/.local/bin/dropbox
@@ -13,11 +15,13 @@ sed -i -e '2s/^/export DISPLAY=\n/' ~/.dropbox-dist/dropboxd
 if [ "$(hostname)" == "marconi" ]; then
     sed -i -e '1s|^#!/usr/bin/python|#!/usr/bin/env python2|' ~/.local/bin/dropbox
 
-    cd /home/cslai/.dropbox-dist/dropbox-lnx*
+    cd $HOME/.dropbox-dist/dropbox-lnx*
 
     patchelf --set-interpreter  $HOME/.linuxbrew/lib/ld.so dropbox
 
+    patchelf --set-rpath '$ORIGIN':$PWD:$HOME/.linuxbrew/lib dropbox
+
     for f in `find . -name "*.so*"`; do
-        patchelf --set-rpath '$ORIGIN':$PWD:/home/cslai/.linuxbrew/lib "$f"
+        patchelf --set-rpath '$ORIGIN':$PWD:$HOME/.linuxbrew/lib "$f"
     done
 fi
