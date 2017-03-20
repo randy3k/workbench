3965ad5ed3fee1a62b84bc0cb486cb5c90c9ce90
Randy Lai <randy.cs.lai@gmail.com>
2017-03-20 00:57:04 -0400

    Update server-bootstrap at Mon Mar 20 00:57:04 EDT 2017

--- julia.sh
+++ julia.sh
@@ -8,12 +8,13 @@ if [[ "$1" = "dev" ]]; then
 else
     URL=$(curl -s "http://julialang.org/downloads/" | sed -n 's/.*href="\([^"]*x86_64\.tar\.gz\)".*/\1/p')
 fi
-echo $URL
-# DIR=`mktemp -d`
-# wget "$URL" -O "$DIR/julia.tar.gz"
-# tar xzfv "$DIR/julia.tar.gz" -C "$DIR"
-# JULIADIR=`find "$DIR" -maxdepth 2 -name "julia*" -type d | head -n 1`
-# [[ -d "$HOME/.local/julia" ]] && rm -rf "$HOME/.local/julia"
-# mv "$JULIADIR" "$HOME/.local/julia"
-# ln -sf "$HOME/.local/julia/bin/julia" "$HOME/.local/bin/julia"
-# rm -r "$DIR"
+echo Downloading from $URL
+
+DIR=`mktemp -d`
+wget "$URL" -O "$DIR/julia.tar.gz"
+tar xzfv "$DIR/julia.tar.gz" -C "$DIR"
+JULIADIR=`find "$DIR" -maxdepth 2 -name "julia*" -type d | head -n 1`
+[[ -d "$HOME/.local/julia" ]] && rm -rf "$HOME/.local/julia"
+mv "$JULIADIR" "$HOME/.local/julia"
+ln -sf "$HOME/.local/julia/bin/julia" "$HOME/.local/bin/julia"
+rm -r "$DIR"
