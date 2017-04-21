c889c158850b8b3c7b9ff82a8899dfff1f506858
Randy Lai <randy.cs.lai@gmail.com>
2017-04-21 10:33:58 -0400

    Update server-bootstrap at Fri Apr 21 10:33:58 EDT 2017

--- iterm.sh
+++ iterm.sh
@@ -4,19 +4,26 @@
 set -e
 
 curl -L https://iterm2.com/misc/`basename $SHELL`_startup.in \
--o ~/.iterm2_shell_integration.`basename $SHELL`
+    -o ~/.local/etc/.iterm2_shell_integration.`basename $SHELL`
+
+UTILITIES=(imgcat imgls it2attention it2check it2copy it2dl it2getvar it2setcolor it2setkeylabel it2ul it2universion)
+for U in "${UTILITIES[@]}"; do
+  echo "Downloading $U..."
+  curl -SsL "https://iterm2.com/utilities/$U" > ~/.local/bin/$U
+  chmod +x ~/.local/bin/$U
+done
 
 if [[ `basename $SHELL` = "bash" ]] && \
     [[ -f ~/.bashrc ]] && \
-    [[ -z `cat ~/.bashrc | grep '.iterm2_shell_integration.bash'` ]]; then
+    [[ -z `cat ~/.bashrc | grep '~/.local/etc/.iterm2_shell_integration.bash'` ]]; then
 cat >> ~/.bashrc <<'EOF'
 
-if [[ -f ~/.iterm2_shell_integration.bash ]]; then
+if [[ -f ~/.local/etc/.iterm2_shell_integration.bash ]]; then
     function iterm2_print_user_vars {
         printf "\033]1337;CurrentDir=''\007"
     }
 
-   source ~/.iterm2_shell_integration.bash
+   source ~/.local/etc/.iterm2_shell_integration.bash
 fi
 EOF
 fi
