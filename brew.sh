e8506585d82d3b62cadcad273b057be9253c2fea
Randy Lai <randy.cs.lai@gmail.com>
2018-03-02 09:50:19 -0500

    update brew.sh

--- brew.sh
+++ brew.sh
@@ -5,17 +5,20 @@ set -e
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
-if [[ -d /home/linuxbrew ]]; then
-    BREWDIR="/home/linuxbrew/.linuxbrew"
-else
-    BREWDIR="$HOME/.linuxbrew"
-fi
 
+if [[ -f ~/.profile ]] && [[ -z `cat ~/.profile | grep 'linuxbrew'` ]]; then
 
-if [[ -f ~/.profile ]] && [[ -z `cat ~/.profile | grep '$HOME/.linuxbrew'` ]]; then
-cat >> ~/.profile <<'EOF'
+cat >> ~/.profile <<EOF
 
 # linuxbrew
+EOF
+
+if [[ -d /home/linuxbrew ]]; then
+    echo BREWDIR="/home/linuxbrew/.linuxbrew" >> ~/.profile
+else
+    echo BREWDIR="$HOME/.linuxbrew" >> ~/.profile
+fi
+cat >> ~/.profile <<'EOF'
 if [ -d "$BREWDIR" ] && [ -z `echo "$PATH" | grep "$BREWDIR"` ]; then
     export PATH="$BREWDIR/bin:$PATH"
     export PATH="$BREWDIR/sbin:$PATH"
