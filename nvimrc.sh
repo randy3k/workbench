20ace065367e264a69ce9fc8823e7b43683dcdb0
Randy Lai <randy.cs.lai@gmail.com>
2018-04-26 14:30:37 -0400

    switch to vim-plug

--- nvimrc.sh
+++ nvimrc.sh
@@ -5,13 +5,9 @@ set -e
 
 curl https://raw.githubusercontent.com/randy3k/dotfiles/master/.nvimrc -o ~/.nvimrc
 
-mkdir -p ~/.config/nvim/bundle
-
+mkdir -p ~/.config/nvim/
 [ -f '~/.config/nvim/init.vim' ] && rm ~/.config/nvim/init.vim
 ln -sf ~/.nvimrc ~/.config/nvim/init.vim
 
-cd ~/.config/nvim/bundle
-
-rm -rf ~/.config/nvim/bundle/Vundle.vim
-
-git clone https://github.com/VundleVim/Vundle.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
