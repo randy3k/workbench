a3604d64d980f1371b63302840283556b85ad900
Randy Lai <randy.cs.lai@gmail.com>
2019-08-26 02:05:56 -0700

    Update server-bootstrap at Mon Aug 26 02:05:56 PDT 2019

--- edit.sh
+++ edit.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-cp -r $HOME/.local/unix-bootstrap $HOME/.local/unix-bootstrap.bk
-rm -rf $HOME/.local/unix-bootstrap
-git clone git@github.com:randy3k/unix-bootstrap.git $HOME/.local/unix-bootstrap
-mv $HOME/.local/unix-bootstrap.bk/* $HOME/.local/unix-bootstrap/
-rm -rf $HOME/.local/unix-bootstrap.bk
+git clone --no-checkout git@github.com:randy3k/unix-bootstrap.git $HOME/.local/unix-bootstrap.tmp
+mv $HOME/.local/unix-bootstrap.tmp/.git $HOME/.local/unix-bootstrap/
+
+rm -rf $HOME/.local/unix-bootstrap.tmp
+cd $HOME/.local/unix-bootstrap
+git reset
