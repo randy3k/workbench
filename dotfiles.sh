10f84203ad4d938dd2f7ba29fb3ebf0c6c2354bd
Randy Lai <randy.cs.lai@gmail.com>
2019-08-28 01:48:13 -0700

    Update server-bootstrap at Wed Aug 28 01:48:13 PDT 2019

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,7 +3,7 @@
 set -e
 shopt -s expand_aliases
 
-git clone --bare $HOME/.dotfiles
+git init --bare $HOME/.dotfiles
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
 dotfiles config --local core.sparseCheckout true
