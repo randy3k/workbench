15ebd7847f66d1becbb465c7fe0b048a3c09e739
Randy Lai <randy.cs.lai@gmail.com>
2019-08-28 02:06:11 -0700

    Update server-bootstrap at Wed Aug 28 02:06:11 PDT 2019

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,7 +8,7 @@ git init --bare $HOME/.dotfiles
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
 dotfiles config --local core.sparseCheckout true
-dotfiles config --local alias.save '!git add $(git diff-files --diff-filter=M --name-only) && git commit -m \"Update dotfiles at $(date -u)\" && git push'
+dotfiles config --local alias.save '!git add -u && git commit -m \"Update dotfiles at $(date -u)\" && git push'
 dotfiles config --local pull.rebase true
 
 dotfiles remote add -f origin git@github.com:randy3k/dotfiles.git
