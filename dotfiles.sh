e52b0cfad7e19250750dcfc164f60a734b7b61d7
Randy Lai <randy.cs.lai@gmail.com>
2019-08-27 19:12:22 -0700

    Update server-bootstrap at Tue Aug 27 19:12:22 PDT 2019

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
 set -e
+shopt -s expand_aliases
 
-
-if [ -d ~/.dotfiles ]; then
-    cd ~/.dotfiles
-    git pull
-    cd -
-else
-    git clone git@github.com:randy3k/dotfiles.git ~/.dotfiles
-fi
-
-mkdir -p ~/.local/etc/
-
-symlink () {
-    rm "$2" 2>/dev/null || true
-    ln -s "$1" "$2"
-}
-
-symlink ~/.dotfiles/.Rprofile ~/.Rprofile
-symlink ~/.dotfiles/.aliases ~/.local/etc/.aliases
-symlink ~/.dotfiles/.bashrc ~/.local/etc/.bashrc
-symlink ~/.dotfiles/.gitconfig ~/.gitconfig
-symlink ~/.dotfiles/.gitignore ~/.gitignore
-symlink ~/.dotfiles/.lintr ~/.lintr
-symlink ~/.dotfiles/.screenrc ~/.screenrc
-symlink ~/.dotfiles/.tmux.conf ~/.tmux.conf
-symlink ~/.dotfiles/.zprofile ~/.zprofile
-symlink ~/.dotfiles/.zshrc ~/.zshrc
-
-alias dotfiles='git --git-dir=$HOME/.dotfiles/.git/ --work-tree=$HOME/.dotfiles/'
+git clone git@github.com:randy3k/dotfiles.git $HOME/.local/dotfiles
+alias dotfiles='git --git-dir=$HOME/.local/dotfiles/.git/ --work-tree=$HOME/.local/dotfiles/'
 dotfiles config --local status.showUntrackedFiles no
-dotfiles config --local alias.save '!git add -u && git commit -m "Update dotfiles at $(date -u)" && git push'
+dotfiles config --local alias.save '!git add $(git diff-files --diff-filter=M --name-only) && git commit -m \"Update dotfiles at $(date -u)\" && git push'
 dotfiles config --local pull.rebase true
+
+dotfiles reset --hard
