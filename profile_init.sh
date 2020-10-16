5461061b2056ed8d72c8078553361255286bc36b
Randy Lai <randy.cs.lai@gmail.com>
2020-10-15 21:38:31 -0700

    update profile_init.sh

--- profile_init.sh
+++ profile_init.sh
@@ -5,11 +5,8 @@ if [[ ! -f ~/.bash_profile ]]; then
     touch ~/.bash_profile
 fi
 
-if [[ -z `cat ~/.bash_profile | grep '## begin workbench'` ]]; then
+if [[ -z $(cat ~/.bash_profile | grep 'source ~/.profile') ]]; then
 cat >> ~/.bash_profile <<'EOF'
-## begin workbench
-## do not edit by hand
-
 if [[ -f ~/.profile ]]; then
     source ~/.profile
 fi
@@ -18,27 +15,32 @@ if [[ -f ~/.bashrc ]]; then
    source ~/.bashrc
 fi
 
-## end workbench
-
 EOF
 fi
 
+
+# bashrc
+if [[ ! -f ~/.bashrc ]]; then
+    touch ~/.bashrc
+fi
+
+if [[ -z $(cat ~/.bashrc | grep '~/.bashrc.d/init.bashrc') ]]; then
+cat >> ~/.bashrc <<'EOF'
+[[ -f ~/.bashrc.d/init.bashrc ]] && . ~/.bashrc.d/init.bashrc
+
+EOF
+fi
 
 # .profile
 if [[ ! -f ~/.profile ]]; then
     touch ~/.profile
 fi
 
-if [[ -z `cat ~/.profile | grep '## begin workbench'` ]]; then
+if [[ -z $(cat ~/.profile | grep '$HOME/.local/bin') ]]; then
 cat >> ~/.profile <<'EOF'
-## begin workbench
-## do not edit by hand
-
 if [[ -d "$HOME/.local/bin" ]] && [[ -z $(echo "$PATH" | grep "$HOME/.local/bin") ]]; then
     export PATH="$HOME/.local/bin:$PATH"
 fi
-
-## end workbench
 
 EOF
 fi
