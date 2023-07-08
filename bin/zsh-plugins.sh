479b26f9e01f586fb98b49296b682d145882b255
Randy Lai <randy.cs.lai@gmail.com>
2023-07-08 11:54:49 -0700

    Update workbench at Sat Jul  8 18:54:49 UTC 2023

--- bin/zsh-plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-git clone https://github.com/esc/conda-zsh-completion ~/.zsh-conda-completion
-git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh-syntax-highlighting
