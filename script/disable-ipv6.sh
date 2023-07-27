7085bb97e70ac4788b5ab4a35187a7b44501dc23
Randy Lai <randy.cs.lai@gmail.com>
2023-07-26 22:37:58 -0700

    Update workbench at Thu Jul 27 05:37:58 UTC 2023

--- script/disable-ipv6.sh
+++ script/disable-ipv6.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if ! grep -Fxq "net.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf; then
     cat <<'EOF' | sudo tee -a /etc/sysctl.conf > /dev/null
 net.ipv6.conf.all.disable_ipv6 = 1
