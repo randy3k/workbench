5c8bfd6116f89ac0dd809cf1beb5a4326e3aa5fe
Randy Lai <randy.cs.lai@gmail.com>
2023-10-16 10:17:24 -0700

    use git clone

--- script/dotfiles.sh
+++ script/dotfiles.sh
@@ -3,21 +3,19 @@
 set -e
 shopt -s expand_aliases
 
-git init --bare $HOME/.dotfiles
+if [[ -n "$SSH_AUTH_SOCK" || -f ~/.ssh/id_rsa ]]; then
+    # has ssh agent forwarded or has ssh key
+    git clone --bare git@github.com:randy3k/dotfiles.git $HOME/.dotfiles
+else
+    git clone --bare https://github.com/randy3k/dotfiles.git $HOME/.dotfiles
+fi
 
 alias dot='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 dot config --local status.showUntrackedFiles no
 dot config --local core.sparseCheckout true
+dot config --local core.sparseCheckoutCone false
 dot config --local pull.rebase true
-
-if [[ -n "$SSH_AUTH_SOCK" || -f ~/.ssh/id_rsa ]]; then
-    # has ssh agent forwarded or has ssh key
-    dot remote add -f origin git@github.com:randy3k/dotfiles.git
-else
-    dot remote add -f origin https://github.com/randy3k/dotfiles.git
-fi
 
 ln -sf ~/.local/workbench/etc/sparse-checkout ~/.dotfiles/info/sparse-checkout
 
 dot checkout master
-dot branch -u origin/master
