1ded30fc088d0ef78f224e79ebdb3eb7cef9cd84
Randy Lai <randy.cs.lai@gmail.com>
2024-09-08 14:49:18 -0700

    fix syntax

--- script/vscode.sh
+++ script/vscode.sh
@@ -5,13 +5,13 @@ set -e
 DIR=$(mktemp -d)
 
 ARCH=$(uname -m)
-if [[ "$ARCH" = "arm" ]]; then
+if [[ "$ARCH" = "aarch64" ]]; then
     URL='https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-arm64'
 elif [[ "$ARCH" = "arm" ]]; then
     URL='https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-arm32'
 else
     URL='https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64'
-end
+fi
 
 curl -Lk $URL --output $DIR/vscode_cli.tar.gz
 tar -xf $DIR/vscode_cli.tar.gz -C ~/.local/bin/
