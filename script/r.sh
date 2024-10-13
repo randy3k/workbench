482780094af57eadb9e242b7cd3f7e299e7d440c
Randy Lai <randy.cs.lai@gmail.com>
2024-10-13 16:43:47 -0700

    update r installation script

--- script/r.sh
+++ script/r.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E19F5F87128899B192B1A2C2AD5F960A256A04AF
+CODENAME=$(grep VERSION_CODENAME /etc/os-release | cut -d'=' -f2)
 
-echo "deb http://cran.rstudio.com/bin/linux/debian stretch-cran35/" | tee /etc/apt/sources.list.d/r-project.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E19F5F87128899B192B1A2C2AD5F960A256A04AF
 
-apt-get upgrade
-apt-get install r-base
+echo "deb http://cloud.r-project.org/bin/linux/debian $CODENAME-cran40/" | tee /etc/apt/sources.list.d/r-project.list
+
+sudo apt-get upgrade
+sudo apt-get install r-base
