cdd785e4f1ead6d125ec63c60fcba893652ed349
Randy Lai <randy.cs.lai@gmail.com>
2026-03-28 20:03:39 -0700

    Update workbench at Sun Mar 29 03:03:39 UTC 2026

--- script/sshkey.sh
+++ script/sshkey.sh
@@ -5,5 +5,5 @@ set -e
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-curl -s https://github.com/randy3k.keys >> ~/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFlSzqV7Bu/ARTcSqjh3A4ND5OESYBmHtOMdRl1Tey8m" >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
